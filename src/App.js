17f46e6a856cff00a9e01a7ea93f4301ead4e91c
ldchinhcr <58549420+ldchinhcr@users.noreply.github.com>
2020-03-26 19:03:08 +0700

    chinh

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
+const clientId = process.env.REACT_APP_CLIENT_ID;
 function App() {
+  const [token,setToken] = useState(null)
+  useEffect(()=> {
+    const existingToken = localStorage.getItem('token');
+    const accessToken = (window.location.search.split("=")[0] === "?access_token") ? window.location.search.split("=")[1] : null;
+  
+    if (!accessToken && !existingToken) {
+      window.location.replace(`https://github.com/login/oauth/authorize?scope=user:email,repo&client_id=${clientId}`)
+    }
+  
+    if (accessToken) {
+      console.log(`New accessToken: ${accessToken}`);
+  
+      localStorage.setItem("token", accessToken);
+      setToken(accessToken)
+    }
+  
+    if (existingToken) {
+      setToken(existingToken)
+    }
+  },[])
+  if (!token) {
+    return( <div>No token to display</div>)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h3>{token}</h3>
     </div>
   );
 }
